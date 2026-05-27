bd4b2054be1c33e60f390bd4665a01273c3758a6
Sandro-Gant <sandro.gantenbein@edu.gbssg.ch>
2026-05-27 16:00:43 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-# Moodle Backup Skript für Modul 169 (Matia & Sandro)
-
 DATE=$(date +"%Y%m%d_%H%M%S")
 BACKUP_DIR="./backup"
 DB_CONTAINER="moodle_db"
 DB_NAME="moodle"
 DB_USER="moodleuser"
 
-source ../.env # Holt das Passwort aus dem Hauptordner
+# Der magische Fix: Wir filtern das Passwort sicher als Text heraus, ohne dass Linux das '>' als Befehl sieht!
+DB_PASS=$(grep '^MYSQL_PASSWORD=' .env | cut -d '=' -f2)
 
 echo "Starte Moodle Backup-Prozess..."
 mkdir -p $BACKUP_DIR
 
 echo "Erstelle Datenbank-Dump..."
-docker exec $DB_CONTAINER /usr/bin/mysqldump -u $DB_USER -p$MYSQL_PASSWORD $DB_NAME > $BACKUP_DIR/db_backup_$DATE.sql
+docker exec $DB_CONTAINER /usr/bin/mysqldump -u $DB_USER -p"$DB_PASS" --no-tablespaces $DB_NAME > $BACKUP_DIR/db_backup_$DATE.sql
 
 echo "Sichere Moodle-Dateien..."
 docker run --rm \
